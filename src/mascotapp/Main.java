abfd1e101347bf459b1d7fbe27aec6b819111c14
Juan Guillermo Marín Díaz <juanguimadi@gmail.com>
2023-06-08 15:08:49 -0500

    new constructors

--- src/mascotapp/Main.java
+++ src/mascotapp/Main.java
@@ -1,11 +1,19 @@
 package mascotapp;
-
+import java.util.Scanner;
 import mascotapp.entidades.Mascota;
 
 public class Main {
     public static void main(String[] args) {
 
-        Mascota m1 = new Mascota();
+        Scanner scan = new Scanner(System.in);
+
+        Mascota m1 = new Mascota("Fernando Grandote", scan.next(), "Perro");
+
+        m1.setNombre("Pepe Grandote");
+
+        m1.pasear(100);
+        System.out.println(m1.toString());
 
     }
+    
 }

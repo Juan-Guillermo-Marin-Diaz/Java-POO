1985aad151b312f5a639081db4db6e7fc38d4d32
Juan Guillermo Marín Díaz <juanguimadi@gmail.com>
2023-06-08 09:29:33 -0500

    First commit

---
+++ src/mascotapp/Main.java
@@ -0,0 +1,10 @@
+package mascotapp;
+
+import mascotapp.entidades.Perro;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Perro p1 = new Perro();
+    }
+}
---
+++ src/mascotapp/entidades/Perro.java
@@ -0,0 +1,10 @@
+package mascotapp.entidades;
+
+public class Perro {
+
+    public String nombre;
+    public String apodo;
+    public String raza;
+    public int edad;
+
+}

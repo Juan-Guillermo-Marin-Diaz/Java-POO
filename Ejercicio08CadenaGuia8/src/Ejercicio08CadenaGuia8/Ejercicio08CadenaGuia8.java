e24ed326c1080be76e54272253f8a96ed8f43706
Juan Guillermo Marín Díaz <juanguimadi@gmail.com>
2023-06-14 18:47:15 -0500

    Cadena class is ready, I think?

---
+++ Ejercicio08CadenaGuia8/src/Ejercicio08CadenaGuia8/Ejercicio08CadenaGuia8.java
@@ -0,0 +1,8 @@
+package Ejercicio08CadenaGuia8;
+
+public class Ejercicio08CadenaGuia8 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Ejercicio08CadenaGuia8/src/Entity/Cadena.java
@@ -0,0 +1,109 @@
+package Entity;
+
+public class Cadena {
+
+    private String frase;
+    private int longitud;
+
+    public Cadena() {
+
+    }
+
+    public Cadena(String frase, int longitud) {
+        this.frase = frase;
+        this.longitud = longitud;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public int setLongitud(int longitud) {
+        this.longitud = longitud;
+
+        return longitud;
+    }
+
+    public int mostrarVocales(String frase){
+
+        int contadorVocales = 0;
+        frase = frase.toLowerCase();
+
+        for (int i = 0; i < frase.length(); i++) {
+
+            char c = frase.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contadorVocales++;
+            }
+
+        }
+        return contadorVocales;
+    }
+
+    public String invertirFrase(String frase) {
+
+        StringBuilder reversa = new StringBuilder(frase);
+        reversa.reverse();
+
+        return reversa.toString();
+    }
+
+    public int vecesRepetido(String letra) {
+
+        int contador = 0;
+
+        for (int i = 0; i < this.frase.length(); i++) {
+
+            if (this.frase.charAt(i) == letra.charAt(0)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public void compararLogintud(String frase, String nuevaFrase) {
+
+        int longitudFrase1 = frase.length();
+        int longitudNuevaFrase = nuevaFrase.length();
+
+        if (longitudFrase1 == longitudNuevaFrase) {
+            System.out.println("La longitud de las frases es igual");
+        } else if (longitudFrase1 > longitudNuevaFrase) {
+            System.out.println("La longitud de la primera frase es mayor que la de la nueva frase");
+        } else {
+            System.out.println("La longitud de la primera frase es menor que la frase nueva");
+        }
+    }
+
+    public String unirFrases(String frase, String nuevaFrase) {
+
+        String fraseUnida = this.frase + " " + nuevaFrase;
+
+        return fraseUnida;
+    }
+
+    public String reemplazar(String letra, String caracterDeReemplazo) {
+
+        String fraseReemplazada  = this.frase.replace(letra, caracterDeReemplazo);
+
+        return fraseReemplazada;
+    }
+
+    public boolean contiene(String letra, char letraABuscar) {
+
+        if (this.frase.equals(letraABuscar)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

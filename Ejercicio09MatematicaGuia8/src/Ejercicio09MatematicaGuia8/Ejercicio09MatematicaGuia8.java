d8cf6c66ff1a76933f2c181856e101b83a1cc87c
Juan Guillermo Marín Díaz <juanguimadi@gmail.com>
2023-06-15 15:46:18 -0500

    Example 09 finalized

--- Ejercicio09MatematicaGuia8/src/Ejercicio09MatematicaGuia8/Ejercicio09MatematicaGuia8.java
+++ Ejercicio09MatematicaGuia8/src/Ejercicio09MatematicaGuia8/Ejercicio09MatematicaGuia8.java
@@ -1,4 +1,18 @@
 package Ejercicio09MatematicaGuia8;
 
+import Entity.Matematica;
+
 public class Ejercicio09MatematicaGuia8 {
+
+    public static void main(String[] args) {
+        Matematica matematica = new Matematica(Math.random(), Math.random());
+
+        System.out.println("El mayor de los números " + matematica.getNumero1() + " y " + matematica.getNumero2() +
+                " es: " + matematica.devolverMayor());
+
+        System.out.println("La potencia del número " + matematica.devolverMayor() + " elevado al número menor es " + matematica.calcularPotencial());
+
+        System.out.println("La raíz cuadrada del número menor es " + matematica.calcularRaiz());
+
+    }
 }
